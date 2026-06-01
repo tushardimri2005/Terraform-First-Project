10014dff267e8c30f02e35ee776c7af164ab577b
tushardimri2005 <tushardimri2005@gmail.com>
2026-06-01 14:45:22 +0530

    Added EC2 user data

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -2,8 +2,30 @@ resource "aws_instance" "ec2_instance" {
   ami           = "ami-0c02fb55956c7d316"
   instance_type = "t3.micro"
 
+  user_data = <<-EOF
+              #!/bin/bash
+
+              yum update -y
+              yum install -y httpd
+
+              systemctl start httpd
+              systemctl enable httpd
+
+              cat > /var/www/html/index.html <<HTML
+              <!DOCTYPE html>
+              <html>
+              <head>
+                <title>DevOps Demo</title>
+              </head>
+              <body>
+                <h1>Website Deployed Automatically 🚀</h1>
+              </body>
+              </html>
+              HTML
+
+              EOF
+
   tags = {
     Name = "Terraform-EC2"
   }
 }
-#This is ec2 file
