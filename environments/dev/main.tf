d2a41bcd60bfd511226af81b67f01d77a1ffdbd2
tushardimri2005 <tushardimri2005@gmail.com>
2026-06-08 11:23:26 +0530

    Ec2 in private subnet

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -38,7 +38,7 @@ module "ec2" {
 
   ami                   = var.ami
   instance_type         = var.instance_type
-  subnet_id             = module.vpc.public_subnet_id
+  subnet_id             = module.vpc.private_subnet_id
   security_group_id     = module.security_group.security_group_id
   key_name              = var.key_name
   instance_profile_name = module.iam.instance_profile_name
